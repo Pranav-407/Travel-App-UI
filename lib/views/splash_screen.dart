08db8d15eab41a15a99d75f8a903301525985c90
Pranav-407 <pranavchavan407@gmail.com>
2025-01-27 17:09:12 +0530

    Splash Screen Done

--- lib/views/splash_screen.dart
+++ lib/views/splash_screen.dart
@@ -8,16 +8,15 @@ class SplashScreen extends StatelessWidget {
     return Scaffold(
       backgroundColor: Color.fromRGBO(13, 110, 253, 1),
       body: Center(
-        // child: Text(
-        //   "Travenor",
-        //   style: TextStyle(
-        //     fontFamily: 'Geometr415BlkBT',
-        //     fontSize: 34,
-        //     fontWeight: FontWeight.w400,
-        //     color: Colors.white
-        //   ),
-        // ),
-        child: Image.asset("assets/images/1.png"),
+        child: Text(
+          "Travenor",
+          style: TextStyle(
+            fontFamily: 'Geometr415BlkBT',
+            fontSize: 34,
+            fontWeight: FontWeight.w400,
+            color: Colors.white
+          ),
+        ),
       ),
     );
   }
