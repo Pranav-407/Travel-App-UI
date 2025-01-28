ef0d8d8d7301e0374be1e26a6f1fb74ae6a9008b
Pranav-407 <pranavchavan407@gmail.com>
2025-01-28 20:31:36 +0530

    Added Constants Fields

---
+++ lib/constants/constants.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+Color black = Color.fromRGBO(27, 30, 40, 1);
+Color grey = Color.fromRGBO(125, 132, 141, 1);
+Color textfield = Color.fromRGBO(247, 247, 249, 1);
+
+final poppins = GoogleFonts.poppins(fontSize: 16, fontWeight: FontWeight.w400, color: black);
+final poppinsHint = GoogleFonts.poppins(fontSize: 16, fontWeight: FontWeight.w400, color: grey);
