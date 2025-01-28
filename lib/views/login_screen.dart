836ba691b0e4bd6958d0cb69648042540eee853e
Pranav-407 <pranavchavan407@gmail.com>
2025-01-28 20:32:43 +0530

    Updated Login Screen with Some UI Part

--- lib/views/login_screen.dart
+++ lib/views/login_screen.dart
@@ -1,13 +1,46 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:travel_app/constants/constants.dart';
 
-class LoginScreen extends StatelessWidget {
+class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
 
   @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('Login Screen'),
+    return Scaffold(
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              SizedBox(width: double.infinity,),
+              Text(
+                "Sign in now",
+                style: GoogleFonts.poppins(
+                  fontSize:26,
+                  fontWeight:FontWeight.w600,
+                  color:black
+                ),
+              ),
+              SizedBox(height: 12),
+              Text(
+                "Please sign in to continue our app",
+                style: GoogleFonts.poppins(
+                  fontSize:16,
+                  fontWeight:FontWeight.w400,
+                  color:grey
+                ),
+              ),
+              SizedBox(height: 40),
+            ],
+          ),
+        ),
       ),
     );
   }
